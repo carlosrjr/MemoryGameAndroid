599097d68dbe8a7cc246c493fd39cfb2ae756529
Carlos Roberto Barbosa Júnior <carlosjr.if@gmail.com>
2021-02-01 18:53:15 -0300

    [ADD] Mudança de cor de fundo, Barra de progresso

--- app/src/main/java/com/pdmsf/memorygame/MainActivity.java
+++ app/src/main/java/com/pdmsf/memorygame/MainActivity.java
@@ -1,11 +1,20 @@
 package com.pdmsf.memorygame;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,11 +25,21 @@ public class MainActivity extends AppCompatActivity {
     private final List<String> listMemory = new ArrayList();
     private final List<Button> matchButton = new ArrayList();
     private int index;
+    private ProgressBar progressBar;
+    private ConstraintLayout bg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        bg = (ConstraintLayout) findViewById(R.id.bgApp);
+        progressBar.setMax(6);
+
+        //setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         resetMemory();
     }
 
@@ -32,22 +51,31 @@ public class MainActivity extends AppCompatActivity {
         showMatchButtons();
         listMemory.clear();
         matchButton.clear();
-
+        bg.setBackgroundColor(getResources().getColor(R.color.teal_200));
+        progressBar.setProgress(0);
         String[] numberArray = this.getResources().getStringArray(R.array.number_array);
         listMemory.addAll(Arrays.asList(numberArray));
         Collections.shuffle(listMemory);
         index = 0;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void selectNumber(View view) {
         Button button = (Button)view;
 
         if(button.getText().equals(listMemory.get(index))) {
+            int color = button.getBackgroundTintList().getDefaultColor();
+
+            bg.setBackgroundColor(color);
+
             matchButton.add(button);
             button.setVisibility(View.INVISIBLE);
             index += 1;
+            progressBar.setProgress(index);
         } else {
+            bg.setBackgroundColor(getResources().getColor(R.color.teal_200));
             index = 0;
+            progressBar.setProgress(0);
             showMatchButtons();
         }
     }
